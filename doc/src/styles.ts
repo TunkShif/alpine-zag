c88afecbfe16fb83d5067c25a7660e63fa9c044a
TunkShif <tunkshif@foxmail.com>
2024-04-17 22:51:35 +0800

    fix: remove unused style

--- doc/src/styles.ts
+++ doc/src/styles.ts
@@ -2,8 +2,7 @@ import { twMerge } from "tailwind-merge"
 
 export const button = (...classNames: string[]) =>
   twMerge(
-    "py-2.5 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm",
-    "hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800",
+    "py-2.5 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50",
     ...classNames
   )
 
