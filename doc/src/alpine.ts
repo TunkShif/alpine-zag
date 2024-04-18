d25eaa55ababd52d9ba61099e3587ca314186ceb
TunkShif <tunkshif@foxmail.com>
2024-04-18 19:28:47 +0800

    doc: component demo style cleanup

--- doc/src/alpine.ts
+++ doc/src/alpine.ts
@@ -1,5 +1,5 @@
 import type { Alpine } from "alpinejs"
-import { zag, createComponent } from "@tunkshif/alpine-zag"
+import { zag } from "@tunkshif/alpine-zag"
 import * as tooltip from "@zag-js/tooltip"
 import * as popover from "@zag-js/popover"
 import * as accordion from "@zag-js/accordion"
@@ -8,14 +8,14 @@ import * as dialog from "@zag-js/dialog"
 import * as combobox from "@zag-js/combobox"
 
 export default (Alpine: Alpine) => {
-  Alpine.plugin(zag)
-
-  Alpine.data("tooltip", createComponent(tooltip.connect, tooltip.machine))
-  Alpine.data("popover", createComponent(popover.connect, popover.machine))
-  Alpine.data("accordion", createComponent(accordion.connect, accordion.machine))
-  Alpine.data("clipboard", createComponent(clipboard.connect, clipboard.machine))
-  Alpine.data("dialog", createComponent(dialog.connect, dialog.machine))
-  Alpine.data("combobox", createComponent(combobox.connect, combobox.machine))
+  Alpine.plugin(
+    zag.register([
+      ["tooltip", tooltip.connect, tooltip.machine],
+      ["popover", popover.connect, popover.machine],
+      ["accordion", accordion.connect, accordion.machine],
+      ["clipboard", clipboard.connect, clipboard.machine],
+      ["dialog", dialog.connect, dialog.machine],
+      ["combobox", combobox.connect, combobox.machine, combobox.collection]
+    ])
+  )
 }
-
-// popover.machine({id: "233", positioning: {}})
