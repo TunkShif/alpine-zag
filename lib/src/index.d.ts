375a0df074e06eb8ad99d75c6c989a474bcd2a09
TunkShif <tunkshif@foxmail.com>
2024-04-19 12:33:13 +0800

    feat: new api
    feat: new create component api
    feat: add x-component directive
    feat: auto sync component context
    doc: updated demo

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -9,14 +9,14 @@ interface ZagPlugin extends PluginCallback {
 
 declare const zag: ZagPlugin
 
-type ComponentConfig = {
-  data: Record<string, unknown>
-  context: Record<string, unknown>
-}
-type CreateComponent = (config: ComponentConfig) => AlpineComponent<unknown>
+type CreateComponent = (
+  initialContext: Record<string, unknown>,
+  syncContext?: Record<string, unknown>
+) => AlpineComponent<unknown>
 type CreateCollection = <T extends CollectionItem>(options: CollectionOptions<T>) => Collection<T>
 
 declare const getComponent: (name: string) => CreateComponent
 declare const getCollection: (name: string) => CreateCollection
 
+export { markRaw } from "@vue/reactivity"
 export { zag, getComponent, getCollection }
