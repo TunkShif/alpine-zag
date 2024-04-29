9d4972a8ca3fd481e2217da668655190b29a0380
TunkShif <tunkshif@foxmail.com>
2024-04-29 22:56:49 +0800

    doc: update component demo

--- docs/src/alpine.ts
+++ docs/src/alpine.ts
@@ -1,8 +1,9 @@
-import { accordion, avatar, carousel } from "@tunkshif/alpine-zag"
+import { accordion, avatar, carousel, collapsible } from "@tunkshif/alpine-zag"
 import type { Alpine } from "alpinejs"
 
 export default (Alpine: Alpine) => {
   Alpine.plugin(accordion)
   Alpine.plugin(avatar)
   Alpine.plugin(carousel)
+  Alpine.plugin(collapsible)
 }
