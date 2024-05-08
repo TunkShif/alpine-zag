7e7a8f87629f867060e0b178de25db4acfd55d80
TunkShif <tunkshif@foxmail.com>
2024-05-08 19:16:11 +0800

    feat: presence animation for hover card

--- lib/src/components/hover-card.ts
+++ lib/src/components/hover-card.ts
@@ -1,8 +1,15 @@
+import { mergeProps } from "@zag-js/core"
 import * as hoverCard from "@zag-js/hover-card"
+import * as presence from "@zag-js/presence"
 import type { Alpine, PluginCallback } from "alpinejs"
 import type { PropTypes } from "src/integration/normalize-props"
-import { createComponent, getApi, handleComponentPart } from "src/utils/create-component"
-import type { CleanupFn } from "src/utils/reactivity"
+import {
+  createBind,
+  createComponent,
+  getApi,
+  handleComponentPart
+} from "src/utils/create-component"
+import { type CleanupFn, markRaw } from "src/utils/reactivity"
 
 export const plugin: PluginCallback = (Alpine) => {
   Alpine.directive("hover-card", (el, directive, { evaluate, cleanup }) => {
@@ -10,9 +17,9 @@ export const plugin: PluginCallback = (Alpine) => {
       case "trigger":
         return handleComponentPart(el, Alpine, "hover_card", "triggerProps")
       case "positioner":
-        return handleComponentPart(el, Alpine, "hover_card", "positionerProps")
+        return handlePositioner(el, Alpine, cleanup)
       case "content":
-        return handleComponentPart(el, Alpine, "hover_card", "contentProps")
+        return handleContent(el, Alpine)
       case "arrow":
         return handleComponentPart(el, Alpine, "hover_card", "arrowProps")
       case "arrow-tip":
@@ -56,4 +63,55 @@ const handleRoot = (el: HTMLElement, Alpine: Alpine, cleanup: CleanupFn, props: 
       return "root"
     }
   })
+}
+
+const handlePositioner = (el: HTMLElement, Alpine: Alpine, cleanup: CleanupFn) => {
+  Alpine.bind(el, {
+    "x-data"() {
+      const ctx = this as any
+      const context = Alpine.reactive({
+        get present() {
+          return ctx.$hoverCard.open
+        }
+      })
+      return createComponent(
+        Alpine,
+        cleanup,
+        "hover_card_presence",
+        context,
+        () => presence.machine({ ...Alpine.raw(context) }),
+        presence.connect
+      )
+    }
+  })
+
+  handleComponentPart(el, Alpine, "hover_card", "positionerProps")
+}
+
+const handleContent = (el: HTMLElement, Alpine: Alpine) => {
+  Alpine.bind(el, {
+    "x-init"() {
+      const ctx = this as any
+      ctx._hover_card_presence_api.value.setNode(el)
+    },
+    "x-data"() {
+      return {
+        get _hoverCardContentProps() {
+          const ctx = this as any
+          const presence = ctx._hover_card_presence_api
+          const localProps = ctx.$hoverCard.contentProps
+          const presenceProps = {
+            hidden: !presence.value.present
+          }
+          return markRaw(mergeProps(localProps, presenceProps))
+        }
+      }
+    }
+  })
+
+  const contentProps = (Alpine.$data(el) as any)._hoverCardContentProps
+  Alpine.bind(
+    el,
+    createBind(contentProps, (ctx) => ctx._hoverCardContentProps)
+  )
 }
